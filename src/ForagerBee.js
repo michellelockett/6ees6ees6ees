9d4999852df973ed450edd39846db2dbe87559f9
Michelle Lockett <shelldog42@gmail.com>
2017-12-07 20:23:40 -0800

    Passes all tests for all bees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor(job, canFly, treasureChest) {
+    super();
+    this.age = 10;
+    this.color = 'yellow';
+    this.canFly = true;
+    this.treasureChest = [];
+    this.job = 'find pollen';
+  }
+
+  forage() {
+    this.treasureChest.push('treasure');
+  }
 };
+
